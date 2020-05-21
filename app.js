278c7c58f83888b9c6d711145873d02b12decafd
Jwana24 <detrieux.btscom13@gmail.com>
2020-05-21 19:05:21 +0200

    add post test and BDD bookmark test

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const connection = require('./connection');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-module.exports = app;
-
-app.get('/', (req,res) => {
+// GET
+app.get('/', (req, res) => {
     res.status(200).json({message: "Hello World!"});
-})
+});
+
+// POST
+app.post('/bookmark', (req, res) => {
+    const { url, title } = req.body;
+    const insertId = req.params.id;
+    
+    if(!url || !title){
+        return res.status(422).json({ error: 'required field(s) missing' });
+    };
+    
+    connection.query('INSERT INTO bookmark SET ?', [req.body], (err, stats) => {
+        if(err){
+            res.status(500).json({
+                error: err.message,
+                sql: err.sql
+            });
+        }
+        else{
+            connection.query('SELECT * FROM bookmark WHERE id = ?', [stats.insertId], (err2, records) => {
+                if(err2){
+                    res.status(500).json({
+                        error: err2.message,
+                        sql: err2.sql
+                    })
+                }
+                else{
+                    res.status(201).json(records[0]);
+                }
+            });
+        }
+    })
+});
+
+module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,12 +1,15 @@
 // test/app.integration.spec.js
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 /*
 méthode regroupant une suite de tests, le 1er paramètre étant le titre du test, puis les tests sont
 écrits dans la fonction qui est le 2e paramètre de la méthode
 */
 describe('Test routes', () => {
+    // vide la table "bookmark" avant chaque test
+    beforeEach(done => connection.query('TRUNCATE bookmark', done));
     // 'it' défini un test avec un titre en 1er paramètre et une fonction en 2e paramètre
     it('GET / sends "Hello World" as json', (done) => {
         // envoi une requête à 'app' d'Express
@@ -26,6 +29,33 @@ describe('Test routes', () => {
             */
             expect(response.body).toEqual(expected);
             // tout est ok, JEST peut finir ici les tests et passer au suivant s'il y a
+            done();
+        });
+    });
+
+    it('POST / Bookmark field(s) missing', (done) => {
+        request(app)
+        .post('/bookmark')
+        .send({})
+        .expect(422)
+        .expect('Content-Type', /json/)
+        .then(res => {
+            const expected = { "error": "required field(s) missing" };
+            expect(res.body).toEqual(expected);
+            done();
+        });
+    });
+
+    it('POST / Bookmark test passed', (done) => {
+        request(app)
+        .post('/bookmark')
+        .send({ url: 'https://jestjs.io', title: 'Jest' })
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .then(res => {
+            // on test la valeur passée dans l'id, peut importe la valeur de type "Number"
+            const expected = { id: expect.any(Number), url: 'https://jestjs.io', title: 'Jest' };
+            expect(res.body).toEqual(expected);
             done();
         });
     });
